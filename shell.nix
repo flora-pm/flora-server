b81c05df17b526f7dbab6c66d1656b6118bc39eb
Théophile Choutri <theophile@choutri.eu>
2021-11-07 16:39:55 +0100

    fix Nix CI

--- shell.nix
+++ shell.nix
@@ -1,12 +1,14 @@
 let pkgs = import (builtins.fetchTarball {
-      # master on 2021-08-01
-      url = "https://github.com/NixOS/nixpkgs/archive/9fc2cddf24ad1819f17174cbae47789294ea6dc4.tar.gz";
-      sha256 = "058l6ry119mkg7pwmm7z4rl1721w0zigklskq48xb5lmgig4l332";
+      # master on 2021-11-07
+      url = "https://github.com/NixOS/nixpkgs/archive/2606cb0fc24e65f489b7d9fdcbf219756e45db35.tar.gz";
     }) { };
 in with pkgs;
   mkShell {
     shellHook = ''
       source environment.sh
+      export LOCALE_ARCHIVE="/nix/store/m53mq2077pfxhqf37gdbj7fkkdc1c8hc-glibc-locales-2.27/lib/locale/locale-archive"
+      export LC_ALL=C.UTF-8
+      export LIBRARY_PATH="${pkgs.zlib}/lib";
     '';
     buildInputs = [
       # Haskell Deps
