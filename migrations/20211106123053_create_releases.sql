8e38b1b15350fb99dc7affc366cd1b4b3d0c33a0
Théophile Choutri <theophile@choutri.eu>
2021-11-13 19:43:17 +0100

    Add release archive checksum

--- migrations/20211106123053_create_releases.sql
+++ migrations/20211106123053_create_releases.sql
@@ -2,6 +2,7 @@ create table releases (
   release_id uuid primary key,
   package_id uuid references packages,
   version text not null,
+  archive_checksum text not null,
   created_at timestamptz,
   updated_at timestamptz,
   unique (package_id, version)
--- src/Flora/Model/Release.hs
+++ src/Flora/Model/Release.hs
@@ -1,18 +1,19 @@
 module Flora.Model.Release where
 
-import Data.Aeson
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Text (Text)
 import Data.Time (UTCTime)
-import Data.UUID
-import Database.PostgreSQL.Entity
-import Database.PostgreSQL.Entity.Types
-import Database.PostgreSQL.Simple
-import Database.PostgreSQL.Simple.FromField
-import Database.PostgreSQL.Simple.ToField
-import Database.PostgreSQL.Transact
+import Data.UUID (UUID)
+import Database.PostgreSQL.Entity (Entity, insert)
+import Database.PostgreSQL.Entity.Types (GenericEntity, TableName)
+import Database.PostgreSQL.Simple (FromRow, ToRow)
+import Database.PostgreSQL.Simple.FromField (FromField)
+import Database.PostgreSQL.Simple.ToField (ToField)
+import Database.PostgreSQL.Transact (DBT)
 import Distribution.Types.Version (Version)
-import GHC.Generics
+import GHC.Generics (Generic)
 
-import Flora.Model.Package
+import Flora.Model.Package (PackageId)
 import Flora.Model.Release.Orphans ()
 
 newtype ReleaseId = ReleaseId { getReleaseId :: UUID }
@@ -20,11 +21,12 @@ newtype ReleaseId = ReleaseId { getReleaseId :: UUID }
     via UUID
 
 data Release = Release
-  { releaseId :: ReleaseId
-  , packageId :: PackageId
-  , version   :: Version
-  , createdAt :: UTCTime
-  , updatedAt :: UTCTime
+  { releaseId       :: ReleaseId
+  , packageId       :: PackageId
+  , version         :: Version
+  , archiveChecksum :: Text
+  , createdAt       :: UTCTime
+  , updatedAt       :: UTCTime
   }
   deriving stock (Eq, Show, Generic)
   deriving anyclass (FromRow, ToRow)
