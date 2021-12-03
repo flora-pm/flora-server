3f62ad8be7936902e6e2a208e991f101027704d9
Théophile Choutri <theophile@choutri.eu>
2021-12-03 09:30:39 +0100

    Enable Sentry integration (#29)

--- environment.sh
+++ environment.sh
@@ -11,3 +11,6 @@ export PG_URI="postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB
 export PG_CONNSTRING="host=${DB_HOST} dbname=${DB_DATABASE} user=${DB_USER} password=${DB_PASSWORD}"
 
 export FLORA_PORT=8083
+
+export FLORA_ENVIRONMENT="local"
+#export SENTRY_DSN="" # Set this variable in `environment.local.sh`, which is not tracked by git.
