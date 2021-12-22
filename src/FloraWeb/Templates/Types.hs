32ba2d3f90f0b4d8ed2016f0df944d33fc9fb48e
Théophile Choutri <theophile@choutri.eu>
2021-12-22 12:20:45 +0100

    Remove the usage of a Map to store template variables (#44)

--- src/FloraWeb/Templates/Types.hs
+++ src/FloraWeb/Templates/Types.hs
@@ -2,31 +2,31 @@ module FloraWeb.Templates.Types where
 
 import Control.Monad.Identity
 import Control.Monad.Reader
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Text
+import Data.Text (Text)
+import GHC.Generics
 import Lucid
 
-type TemplateEnv = ReaderT TemplateAssigns Identity
+type FloraHTML = HtmlT (ReaderT TemplateEnv Identity) ()
 
-type FloraHTML = HtmlT TemplateEnv ()
+newtype FlashInfo = FlashInfo { getFlashInfo :: Text }
+  deriving Show via Text
+newtype FlashError = FlashError { getFlashInfo :: Text }
+  deriving Show via Text
 
-newtype TemplateAssigns = TemplateAssigns { getAssigns :: Map Text Text }
-  deriving newtype (Show, Eq)
+data TemplateEnv = TemplateEnv
+  { displayNavbarSearch :: Bool
+  , flashInfo           :: Maybe FlashInfo
+  , flashError          ::Maybe FlashError
+  , title               :: Text
+  , description         :: Text
+  }
+  deriving stock (Show, Generic)
 
-newtype UserAssigns = UserAssigns {getUserAssigns :: Map Text Text}
-  deriving newtype (Show, Eq)
-
-emptyAssigns :: TemplateAssigns
-emptyAssigns = TemplateAssigns Map.empty
-
-mkAssigns :: TemplateAssigns -> Maybe UserAssigns -> TemplateAssigns
-mkAssigns (TemplateAssigns templateAssigns) (Just (UserAssigns userAssigns)) =
-  TemplateAssigns $ Map.union templateAssigns userAssigns
-mkAssigns ta Nothing = ta
-
-getTA :: TemplateAssigns -- ^  Template Assigns
-      -> Text -- ^ Default value if the key is not present
-      -> Text -- ^ Key
-      -> Text
-getTA (TemplateAssigns templateAssigns) defaultValue key = Map.findWithDefault defaultValue key templateAssigns
+defaultTemplateEnv :: TemplateEnv
+defaultTemplateEnv = TemplateEnv
+  { displayNavbarSearch = True
+  , flashInfo = Nothing
+  , flashError = Nothing
+  , title = "Flora :: [Package]"
+  , description = "Package index for the Haskell ecosystem"
+  }
