3026c8aba373e70b6b7c4cb93dac81ee3ebd2145
Théophile Choutri <theophile@choutri.eu>
2022-02-03 13:26:30 +0100

    Remove nix-based build system in favour of cabal

--- nix/ci.nix
+++
@@ -1,6 +0,0 @@
-let pkgs = import ./pkgs.nix;
-in {
-  default = pkgs.haskellPackages.flora-server;
-  shell = (import ../shell.nix { inherit pkgs; });
-  test = import ./test.nix { };
-}
--- shell.nix
+++ shell.nix
@@ -1,9 +1,8 @@
 {
 # https://github.com/NixOS/nixpkgs/blob/master/pkgs/top-level/haskell-packages.nix
-pkgs ? import ./nix/pkgs.nix }:
-pkgs.haskellPackages.shellFor {
-  packages = ps: [ ps.flora-server ];
-  buildInputs = [
+pkgs ? import ./nix/pin.nix {}}:
+pkgs.mkShell {
+  nativeBuildInputs = [
     pkgs.ghcid
     pkgs.cabal-install
     pkgs.hlint
@@ -27,8 +26,6 @@ pkgs.haskellPackages.shellFor {
     source environment.sh
     export LOCALE_ARCHIVE="/nix/store/m53mq2077pfxhqf37gdbj7fkkdc1c8hc-glibc-locales-2.27/lib/locale/locale-archive"
     export LC_ALL=C.UTF-8
-    printf "To start the server: \'make nix-build && make nix-start\'\n"
-    printf "To start the assets pipeline: \'make assets-watch\'\n"
-    printf "Happy hacking!\n"
+    cat scripts/nix-welcome.txt
   '';
 }
