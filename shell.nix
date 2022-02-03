3163f768c11e7e95fbf278d85afea464357a286d
Théophile Choutri <theophile@choutri.eu>
2022-02-03 14:27:32 +0100

    fixup! fixup! fixup! fixup! Remove nix-based build system in favour of cabal

--- shell.nix
+++ shell.nix
@@ -1,28 +1,32 @@
 {
 # https://github.com/NixOS/nixpkgs/blob/master/pkgs/top-level/haskell-packages.nix
-pkgs ? import ./nix/pin.nix {}}:
-pkgs.mkShell {
+pkgs ? import ./nix/pin.nix { } }:
+pkgs.mkShell rec {
   nativeBuildInputs = [
-    pkgs.ghcid
+    pkgs.bash
     pkgs.cabal-install
-    pkgs.hlint
     pkgs.cacert
-    pkgs.haskellPackages.apply-refact
-    pkgs.stylish-haskell
-    pkgs.git
-    pkgs.haskellPackages.cabal-fmt
-    pkgs.postgresql
-    pkgs.yarn
     pkgs.concurrently
     pkgs.esbuild
+    pkgs.ghcid
+    pkgs.git
+    pkgs.haskellPackages.apply-refact
+    pkgs.haskellPackages.cabal-fmt
+    pkgs.hlint
     pkgs.iputils
-    pkgs.tmux
-    pkgs.bash
+    pkgs.ncurses6
     pkgs.nixfmt
+    pkgs.postgresql_14
+    pkgs.stylish-haskell
+    pkgs.tmux
+    pkgs.yarn
+    pkgs.libffi
+    pkgs.zlib
   ];
   exactDeps = true;
   NIX_PATH = "nixpkgs=${pkgs.path}:.";
   shellHook = ''
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath nativeBuildInputs}"
     source environment.sh
     export LOCALE_ARCHIVE="/nix/store/m53mq2077pfxhqf37gdbj7fkkdc1c8hc-glibc-locales-2.27/lib/locale/locale-archive"
     export LC_ALL=C.UTF-8
