c332a90b82cc5867709c61965627d1b561473060
Théophile Choutri <theophile@choutri.eu>
2022-02-12 21:02:22 +0100

    fixup! Categories import

--- src/Flora/Import/Categories.hs
+++ src/Flora/Import/Categories.hs
@@ -1,6 +1,24 @@
-import Flora.Import.Categories where
+{-# OPTIONS_GHC -Wno-unused-matches #-}
+{-# OPTIONS_GHC -Wno-unused-local-binds #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+module Flora.Import.Categories where
 
-import Tomland
+import Toml
+import qualified Toml
+import qualified Data.UUID.V4 as UUID
+import Flora.Model.Category.Types (CategoryId (CategoryId))
+import qualified Data.List.NonEmpty as NonEmpty
+-- import Data.Map (Map)
+-- import Flora.Model.Category.Types
 
-importCategories :: IO ()
-importCategories = do
+-- importCategories :: IO ()
+-- importCategories = undefined
+
+
+createCategory :: (Key, TOML) -> IO CategoryId
+createCategory (key, body) = do
+  categoryId <- CategoryId <$> UUID.nextRandom
+  let slug = NonEmpty.head $ unKey key
+  let pairs = Toml.toList $ Toml.tomlTables body
+  let name = undefined
+  pure undefined
