9d901a8cb784ac170987cbd15969a4bee2aab158
Théophile Choutri <theophile@choutri.eu>
2022-04-20 17:10:10 +0200

    Fix tmux

--- scripts/start-tmux.sh
+++ scripts/start-tmux.sh
@@ -8,6 +8,6 @@ sleep 1
 tmux send-keys -t "flora" 'make watch-server' 'C-m'
 tmux select-window -t flora:0
 sleep 1
-tmux split-window -h 'make assets-watch'
+tmux split-window -h 'make watch-assets'
 tmux send-keys -t "flora" 'C-b'
 tmux attach-session -t flora
