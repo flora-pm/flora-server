c5d1123488532ce2680de49be096ca90a207e14d
Théophile Choutri <theophile@choutri.eu>
2022-05-17 20:16:27 +0200

    fixup! Move the design system

--- design/stories/category-card.stories.js
+++ design/stories/category-card.stories.js
@@ -1,7 +1,6 @@
  
-    export default {
-      title: 'Example/Headings'
-    };
+export default {
+  title: "Category/Card"
+};
 
-    export const Category/Card = () => <a class="card" href="/categories/prelude"><h2 class="font-semibold text-brand-purple dark:text-brand-purple-light">Prelude</h2><p class="mt-2 text-neutral-900 dark:text-gray-200">Libraries that provide default imports</p></a>
-    
+export const CategoryCard = () => "<a class="card" href="/categories/prelude"><h2 class="font-semibold text-brand-purple dark:text-brand-purple-light">Prelude</h2><p class="mt-2 text-neutral-900 dark:text-gray-200">Libraries that provide default imports</p></a>"
