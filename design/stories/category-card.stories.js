e5b00858c3b524e7ca37cf4d4d39c2ea53e3c997
Théophile Choutri <theophile@choutri.eu>
2022-05-22 12:54:50 +0200

    Regen design system

--- design/stories/category-card.stories.js
+++ design/stories/category-card.stories.js
@@ -3,4 +3,4 @@ export default {
   title: "Components/Category"
 };
 
-export const CategoryCard = () => "<div class=\"category-card\"><a class href=\"/categories/prelude\"><h2 class=\"category-card__name font-semibold text-purple\">Prelude</h2><p class=\"category-card__synopsis mt-2\">Libraries that provide default imports</p></a></div>"
+export const CategoryCard = () => "<div class><a class=\"category-card\" href=\"/categories/prelude\"><h2 class=\"category-card__name font-semibold text-purple\">Prelude</h2><p class=\"category-card__synopsis mt-2\">Libraries that provide default imports</p></a></div>"
