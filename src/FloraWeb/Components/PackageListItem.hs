53f8b2e966f48348abd060a52b4b4a3f4dd6baba
Théophile Choutri <theophile@choutri.eu>
2022-05-22 18:17:55 +0200

    Fix light theme

--- src/FloraWeb/Components/PackageListItem.hs
+++ src/FloraWeb/Components/PackageListItem.hs
@@ -13,12 +13,12 @@ import Flora.Model.Package (Namespace, PackageName)
 packageListItem :: (Namespace, PackageName, Text, Text) -> FloraHTML
 packageListItem (namespace, name, synopsis, version) = do
   let href = href_ ("/packages/@" <> display namespace <> "/" <> display name)
-  li_ [class_ "package-list-item xl:text-xl"] $
+  li_ [class_ "package-list-item"] $
     a_ [href, class_ "block md:my-6"] $ do
       h4_ [class_ "package-list-item__name"] $
         strong_ [class_ ""] . toHtml $ prettyPackageName namespace name
       p_ [class_ "package-list-item__synopsis"] $ toHtml synopsis
-      div_ [class_ "package-list-item__version text-sm mt-2"] $ toHtml version
+      div_ [class_ "package-list-item__version"] $ toHtml version
 
 prettyPackageName :: Namespace -> PackageName -> Text
 prettyPackageName namespace name = "@" <> display namespace <> "/" <> display name
