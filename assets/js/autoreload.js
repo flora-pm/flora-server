fe147b5952d25ced1e7fca9c2a5167f3c57e9358
Théophile Choutri <theophile@choutri.eu>
2022-08-06 10:26:12 +0200

    Add a margin-top for h{2..4} tags and make <p> inline inside <li>

--- assets/js/autoreload.js
+++ assets/js/autoreload.js
@@ -6,33 +6,37 @@ var wasConnectionLost = false;
 
 const connectToServer = () => {
   sessionStorage.setItem("connId", null);
-  console.log("[+] Connecting to the autoreload websocket…")
-  const autoreloadSocket = new WebSocket("ws://localhost:8083/sockets/autoreload");
-  autoreloadSocket.addEventListener('close',  onWebsocketClose);
-  autoreloadSocket.addEventListener('error', onWebsocketClose);
-  autoreloadSocket.addEventListener('open',  (event) => onWebsocketOpen(autoreloadSocket, event));
-}
+  console.log("[+] Connecting to the autoreload websocket…");
+  const autoreloadSocket = new WebSocket(
+    "ws://localhost:8083/sockets/autoreload"
+  );
+  autoreloadSocket.addEventListener("close", onWebsocketClose);
+  autoreloadSocket.addEventListener("error", onWebsocketClose);
+  autoreloadSocket.addEventListener("open", (event) =>
+    onWebsocketOpen(autoreloadSocket, event)
+  );
+};
 
 const onWebsocketOpen = (socket, event) => {
   console.log("[+] Connection to server established!");
   if (wasConnectionLost) {
     location.reload(true);
   }
-}
+};
 
 const onWebsocketClose = () => {
   console.log("[!] Server disconnected, attempting to reconnect…");
   wasConnectionLost = true;
   setTimeout(() => {
-      reconnectToWebsocket();
+    reconnectToWebsocket();
   }, currentReconnectDelay);
-}
+};
 
 const reconnectToWebsocket = () => {
-  if(currentReconnectDelay < maxReconnectDelay) {
-      currentReconnectDelay*=2;
+  if (currentReconnectDelay < maxReconnectDelay) {
+    currentReconnectDelay *= 2;
   }
   connectToServer();
-}
+};
 
 connectToServer();
