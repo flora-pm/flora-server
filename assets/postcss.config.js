5e36a681eec4e3c1b9b51b4f122b72dfcaef5e59
Théophile Choutri <theophile@choutri.eu>
2022-08-06 12:42:41 +0200

    Add a margin-top for h{2..4} tags and make <p> inline inside <li> (#165)

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   plugins: [
-    require("postcss-import")(),
+    require("postcss-import") (),
     require("@tailwindcss/nesting"),
-    require("tailwindcss")(),
-    require("autoprefixer")(),
-    require("postcss-copy")({
+    require("tailwindcss") (),
+    require("autoprefixer") (),
+    require("postcss-copy") ({
       dest: "../assets/fonts",
     }),
-    ...(process.env.ENV_BUILD === "prod" ? [purgecss] : []),
-    ...(process.env.ENV_BUILD === "prod" ? [require("cssnano")()] : []),
+    ...(process.env.env_build === "prod" ? [purgecss] : []),
+    ...(process.env.env_build === "prod" ? [require("cssnano") () ] : []),
   ],
 };
