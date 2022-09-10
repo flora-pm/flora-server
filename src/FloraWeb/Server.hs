25b26747b4cbcb5e726f30379c3dc4f734a96249
Théophile Choutri <theophile@choutri.eu>
2022-09-10 15:46:48 +0200

    Implement OpenSearch (#192)
    * Implement OpenSearch
    
    Fix #127
    
    * lint

--- src/FloraWeb/Server.hs
+++ src/FloraWeb/Server.hs
@@ -58,6 +58,7 @@ import FloraWeb.Server.Auth (FloraAuthContext, authHandler, runVisitorSession)
 import FloraWeb.Server.Logging (runLog)
 import FloraWeb.Server.Logging qualified as Logging
 import FloraWeb.Server.Metrics
+import FloraWeb.Server.OpenSearch
 import FloraWeb.Server.Pages qualified as Pages
 import FloraWeb.Server.Tracing
 import FloraWeb.Types
@@ -137,6 +138,7 @@ floraServer :: Pool Connection -> OddJobs.UIConfig -> OddJobs.Env -> Routes (AsS
 floraServer pool cfg jobsRunnerEnv =
   Routes
     { assets = serveDirectoryWebApp "./static"
+    , openSearch = openSearchHandler
     , pages = \sessionWithCookies ->
         hoistServerWithContext
           (Proxy @Pages.Routes)
