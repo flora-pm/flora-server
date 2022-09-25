42b12e7a9607c5ea84fd486afdca8ad6eb031351
Théophile Choutri <theophile@choutri.eu>
2022-09-25 20:19:23 +0200

    Revert "Maintain the site title static"
    This reverts commit cddc5c072b476cc7ab0e77f348886b128f7f7ccd.

--- src/FloraWeb/Components/Header.hs
+++ src/FloraWeb/Components/Header.hs
@@ -14,6 +14,7 @@ import FloraWeb.Templates.Types (FloraHTML, TemplateEnv (..))
 
 header :: FloraHTML
 header = do
+  TemplateEnv{title} <- ask
   doctype_
   html_
     [ lang_ "en"
@@ -31,7 +32,7 @@ header = do
       head_ $ do
         meta_ [charset_ "UTF-8"]
         meta_ [name_ "viewport", content_ "width=device-width, initial-scale=1"]
-        title_ (text "Flora :: [Package]")
+        title_ (text title)
 
         script_ [type_ "module"] $ do
           toHtmlRaw @Text
