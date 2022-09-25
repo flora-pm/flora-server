c4060fc462e79f296263ee22f8ac3be4321ecd0c
Théophile Choutri <theophile@choutri.eu>
2022-09-25 17:15:58 +0200

    [FLORA-169] Colourise the search bars on focus
    fix #169

--- design/stories/package-list--item.stories.js
+++ design/stories/package-list--item.stories.js
@@ -3,4 +3,4 @@ export default {
   title: "Components/Package List"
 };
 
-export const PackageListItem = () => "<li class=\"package-list-item\"><a href=\"/packages/@haskell/base\" class=\"block md:my-6\"><h4 class=\"package-list-item__name\"><strong class>@haskell/base</strong></h4><p class=\"package-list-item__synopsis\">Basic libraries</p><div class=\"package-list-item__version\">4.16.0.0</div></a></li>"
+export const PackageListItem = () => "<li class=\"package-list-item\"><a href=\"/packages/@haskell/base\" class><h4 class=\"package-list-item__name\"><strong class>@haskell/base</strong></h4><p class=\"package-list-item__synopsis\">Basic libraries</p><div class=\"package-list-item__version\">4.16.0.0</div></a></li>"
