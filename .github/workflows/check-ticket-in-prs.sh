18342d8c147739e63b45c3549cd9b840166eea64
Théophile Choutri <theophile@choutri.eu>
2022-11-10 11:02:33 +0100

    [NO-ISSUE] Fix the name checking in check-ticket-in-prs.sh

--- .github/workflows/check-ticket-in-prs.sh
+++ .github/workflows/check-ticket-in-prs.sh
@@ -2,13 +2,16 @@
 
 COMMIT_MESSAGE_PATTERN="\[((FLORA)-[0-9]+)|NO-ISSUE\]"
 
-if [ "$PULL_REQUEST_AUTHOR" == "dependabot" ]; then
-  echo "Pull request by dependabot - not checking"
-  exit 0
-else
-  echo "$PULL_REQUEST_TITLE" | grep -E "${COMMIT_MESSAGE_PATTERN}"
-  if [ $? -ne 0 ]; then
-    echo "Pull request title doesn't start with '[FLORA-ISSUE]' or '[NO-ISSUE]'" > /dev/stderr
-    exit 1
-  fi
-fi
+case "$PULL_REQUEST_AUTHOR"  ] in
+  "dependabot[bot]" | "dependabot" )
+    echo "Pull request by dependabot - not checking"
+    exit 0
+    ;;
+      
+  *)
+    echo "$PULL_REQUEST_TITLE" | grep -E "${COMMIT_MESSAGE_PATTERN}"
+    if [ $? -ne 0 ]; then
+      echo "Pull request title doesn't start with '[FLORA-ISSUE]' or '[NO-ISSUE]'" > /dev/stderr
+      exit 1
+    fi
+    ;;
