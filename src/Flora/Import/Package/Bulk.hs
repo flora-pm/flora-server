4f25ae526b459891ff75f0d1501736a7854a5dde
Théophile Choutri <theophile@choutri.eu>
2022-12-01 09:38:23 +0100

    [NO-ISSUE] Save the profiling settings and refine the freeze file

--- src/Flora/Import/Package/Bulk.hs
+++ src/Flora/Import/Package/Bulk.hs
@@ -14,7 +14,6 @@ import Streamly.Prelude qualified as S
 import System.Directory qualified as System
 import System.FilePath
 
-import Control.Concurrent qualified as Concurrent
 import Flora.Import.Package (loadAndExtractCabalFile, persistImportOutput)
 import Flora.Model.Package.Update qualified as Update
 import Flora.Model.Release.Update qualified as Update
@@ -40,8 +39,7 @@ importAllFilesInDirectory appLogger user dir = do
                   when (currentCount `mod` 400 == 0) $
                     displayStats currentCount
                   return currentCount
-  threadCount <- liftIO Concurrent.getNumCapabilities
-  processedPackageCount <- liftIO $ S.fold displayCount $ S.fromParallel $ S.mapM (processFile pool) $ S.maxThreads (threadCount - 1) $ findAllCabalFilesInDirectory dir
+  processedPackageCount <- liftIO $ S.fold displayCount $ S.fromParallel $ S.mapM (processFile pool) $ findAllCabalFilesInDirectory dir
   displayStats processedPackageCount
   Update.refreshLatestVersions >> Update.refreshDependents
   where
