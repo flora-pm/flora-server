b570686780a381b7595253280ec5243dd695c44b
Daniel Firth <dan.firth@homotopic.tech>
2022-12-31 16:17:09 +0000

    nix flake update (#316)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -2,27 +2,25 @@
   description = "flora";
   inputs = {
     flake-utils = { url = "github:numtide/flake-utils"; };
-    horizon-platform = { url = "github:blackheaven/horizon-platform-forked"; };
+    horizon-platform = { url = "git+https://gitlab.homotopic.tech/horizon/horizon-platform"; };
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
   outputs = inputs@{ self, flake-utils, horizon-platform, nixpkgs, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        hsPkgs = pkgs.haskell.packages.ghc942.override {
-          overrides = hfinal: hprev:
-            with pkgs.haskell.lib;
-            horizon-platform.packages.${system} // {
-              flora = overrideCabal (dontHaddock (dontCheck
-                (disableLibraryProfiling
-                  (doJailbreak (hfinal.callCabal2nix "flora" ./. { })))))
-                (drv: {
-                  preConfigure = ''
-                    cd cbits; ${pkgs.souffle}/bin/souffle -g categorise.{cpp,dl}
-                    cd ..
-                  '';
-                });
-            };
+        hsPkgs = with pkgs.haskell.lib;
+          horizon-platform.legacyPackages.${system}.override {
+            overrides = hfinal: hprev: {
+                flora = overrideCabal (dontHaddock (dontCheck
+                    (doJailbreak (hfinal.callCabal2nix "flora" ./. { }))))
+                  (drv: {
+                    preConfigure = ''
+                      cd cbits; ${pkgs.souffle}/bin/souffle -g categorise.{cpp,dl}
+                      cd ..
+                    '';
+                  });
+              };
         };
       in {
         apps = rec {
