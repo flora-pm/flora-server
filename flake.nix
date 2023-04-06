2aacc7e61c72fe64d72801067bfea99e9931c7d3
Johannes Maier <johannes.maier@mailbox.org>
2023-04-06 22:18:59 +0200

    [NO-ISSUE] Clean up flake.nix a tiny bit
    - Use `foo.url` when it's the only attribute, a set otherwise
    - Use the github:owner/repo style for GitHub exclusively

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   description = "flora";
   inputs = {
-    flake-utils = { url = "github:numtide/flake-utils"; };
-    horizon-platform = {
-      url =
-        "git+https://gitlab.horizon-haskell.net/package-sets/horizon-platform";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
+    horizon-platform.url =
+      "git+https://gitlab.horizon-haskell.net/package-sets/horizon-platform";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    streamly.url = "git+https://github.com/composewell/streamly";
-    streamly.flake = false;
-    poolboy.url = "git+https://github.com/blackheaven/poolboy";
+    streamly = {
+      url = "github:composewell/streamly";
+      flake = false;
+    };
+    poolboy.url = "github:blackheaven/poolboy";
   };
   outputs = inputs@{ self, flake-utils, horizon-platform, nixpkgs, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
@@ -17,7 +17,7 @@
         pkgs = import nixpkgs { inherit system; };
         hsPkgs = with pkgs.haskell.lib;
           horizon-platform.legacyPackages.${system}.override {
-            overrides = hfinal: hprev: rec {
+            overrides = hfinal: hprev: {
               flora = overrideCabal (dontHaddock (dontCheck
                 (doJailbreak (hfinal.callCabal2nix "flora" ./. { })))) (drv: {
                   preConfigure = ''
@@ -38,13 +38,11 @@
 
           server = {
             type = "app";
-
             program = "${hsPkgs.flora}/bin/flora-server";
           };
 
           cli = {
             type = "app";
-
             program = "${hsPkgs.flora}/bin/flora-cli";
           };
         };
