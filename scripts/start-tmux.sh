ae795fc390e02dd10e3994c64ea5deea3147b590
Johannes Maier <johannes.maier@mailbox.org>
2023-04-07 14:20:04 +0200

    [FLORA-368] Do `yarn install` to unbreak `make start-tmux`

--- scripts/start-tmux.sh
+++ scripts/start-tmux.sh
@@ -8,6 +8,7 @@ sleep 1
 tmux send-keys -t "flora" 'make watch-server' 'C-m'
 tmux select-window -t flora:0
 sleep 1
+(cd assets && yarn install)
 tmux split-window -h 'make watch-assets'
 tmux send-keys -t "flora" 'C-b'
 tmux attach-session -t flora
