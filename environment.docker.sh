dcd7fcfea21814339b850d9aedfb3c84f63681b2
Johannes Maier <kenranunderscore@users.noreply.github.com>
2023-04-07 22:59:24 +0200

    [FLORA-368] Upgrade and unbreak docker setup for GHC 9.4.4 (#370)

---
+++ environment.docker.sh
@@ -0,0 +1,6 @@
+source environment.sh
+
+export FLORA_DB_HOST="database"
+export FLORA_HTTP_PORT=8084
+export FLORA_DB_CONNSTRING="host=${FLORA_DB_HOST} dbname=${FLORA_DB_DATABASE}\
+  user=${FLORA_DB_USER} password=${FLORA_DB_PASSWORD}"
