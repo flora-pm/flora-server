d7b6ce81966df9b71924b37da2ff516439340f74
mangoiv <mail@mangoiv.com>
2023-04-07 14:16:47 +0200

    [fix] fix nix - change some deps to work with package set - bump version of souffle - some general lints - nix fmt

--- flake.nix
+++ flake.nix
@@ -1,68 +1,62 @@
 {
   description = "flora";
+  nixConfig = {
+    extra-substituters = "https://horizon.cachix.org";
+    extra-trusted-public-keys = "horizon.cachix.org-1:MeEEDRhRZTgv/FFGCv3479/dmJDfJ82G6kfUDxMSAw0=";
+  };
+
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     horizon-platform.url =
       "git+https://gitlab.horizon-haskell.net/package-sets/horizon-platform";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    # nixos-22.05 provides souffle 2.2; that version does not easily
-    # build on the current pin, so use a separate input for this
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-22.05";
-    streamly = {
-      url = "github:composewell/streamly";
-      flake = false;
-    };
-    poolboy.url = "github:blackheaven/poolboy";
+
+    # non-nix dependencies
+    streamly.url = "github:composewell/streamly/81bfdec";
+    streamly.flake = false;
+    poolboy.url = "github:blackheaven/poolboy/v0.2.1.0";
+    poolboy.flake = false;
   };
-  outputs = inputs@{ self, flake-utils, horizon-platform, nixpkgs, ... }:
+  outputs = inputs@{ flake-utils, horizon-platform, nixpkgs, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        souffle_2_2 = inputs.nixpkgs-stable.legacyPackages.${system}.souffle;
+        pkgs = nixpkgs.legacyPackages.${system};
         hsPkgs = with pkgs.haskell.lib;
           horizon-platform.legacyPackages.${system}.override {
             overrides = hfinal: hprev: {
-              flora = overrideCabal (dontHaddock (dontCheck
-                (doJailbreak (hfinal.callCabal2nix "flora" ./. { })))) (drv: {
-                  preConfigure = ''
-                    cd cbits
-                    ${pkgs.lib.getExe souffle_2_2} -g categorise.{cpp,dl}
-                    cd ..
-                  '';
-                });
+              flora = dontCheck (overrideCabal (hfinal.callCabal2nix "flora" ./. { }) (drv: {
+                preConfigure = ''
+                  ${pkgs.lib.getExe pkgs.souffle} -g cbits/categorise.{cpp,dl}
+                '';
+              }));
               streamly-core =
-                hfinal.callCabal2nix "streamly-core" "${inputs.streamly}/core/"
-                { };
+                hfinal.callCabal2nix "streamly-core" "${inputs.streamly}/core/" { };
               streamly = hfinal.callCabal2nix "streamly" inputs.streamly { };
-              poolboy = hfinal.callCabal2nix "poolboy" inputs.poolboy { };
+              poolboy = dontCheck (hfinal.callCabal2nix "poolboy" inputs.poolboy { });
+              resource-pool = hfinal.callHackage "resource-pool" "0.3.1.0" { };
             };
           };
-      in {
+      in
+      {
         apps = rec {
           default = server;
-
-          server = {
-            type = "app";
-            program = "${hsPkgs.flora}/bin/flora-server";
-          };
-
-          cli = {
-            type = "app";
-            program = "${hsPkgs.flora}/bin/flora-cli";
-          };
+          server = flake-utils.lib.mkApp { drv = "${hsPkgs.flora}/bin/flora-server"; };
+          cli = flake-utils.lib.mkApp { drv = "${hsPkgs.flora}/bin/flora-cli"; };
         };
+        formatter = pkgs.nixpkgs-fmt;
         devShells.default = hsPkgs.shellFor {
           packages = p: [ p.flora ];
-          nativeBuildInputs = with pkgs; [
-            cabal-install
-            ghcid
-            hlint
+          nativeBuildInputs = [
             hsPkgs.apply-refact
             hsPkgs.fourmolu
             hsPkgs.postgresql-migration
-            postgresql_14
-            souffle_2_2
+            pkgs.cabal-install
+            pkgs.ghcid
+            pkgs.hlint
+            pkgs.postgresql_14
+            pkgs.souffle
           ];
+
           shellHook = ''
             source ./environment.sh
             cat ./scripts/shell-welcome.txt
