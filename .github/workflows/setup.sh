0884079c24a29f28c2a43adc923e33ccc1235f72
Théophile Choutri <theophile@choutri.eu>
2023-04-08 14:37:48 +0200

    Fix llvm dep

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -3,4 +3,4 @@
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt -y update
-sudo apt -y install postgresql-14 g++ mcpp libffi7 llvm14
+sudo apt -y install postgresql-14 g++ mcpp libffi7 llvm-14
