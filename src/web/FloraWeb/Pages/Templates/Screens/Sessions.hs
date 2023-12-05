be3183db1ad09e895bc8e01d6a1723e5cc562b7e
Théophile Choutri <theophile@choutri.eu>
2023-12-05 18:13:17 +0100

    Finish the form

--- src/web/FloraWeb/Pages/Templates/Screens/Sessions.hs
+++ src/web/FloraWeb/Pages/Templates/Screens/Sessions.hs
@@ -8,26 +8,40 @@ newSession = do
   let formClasses = "login-form"
   form_ [action_ "/sessions/new", method_ "POST", class_ formClasses] $ do
     h2_ [class_ ""] "Sign in"
-    div_ $ do
-      label_ [for_ "email", class_ "sr-only"] "Email address"
+    label_ [for_ "email", class_ "sr-only"] "Email address"
+    input_
+      [ id_ "email"
+      , name_ "email"
+      , type_ "email"
+      , autocomplete_ "email"
+      , required_ ""
+      , placeholder_ "Email address"
+      , class_ "form-input"
+      ]
+    label_ [for_ "password", class_ "sr-only"] "Email address"
+    input_
+      [ id_ "password"
+      , name_ "password"
+      , type_ "password"
+      , autocomplete_ "current-password"
+      , required_ ""
+      , placeholder_ "Password"
+      , class_ "form-input"
+      ]
+    label_ [for_ "use_totp"] "Use two-factor authentication"
+    input_
+      [ id_ "use_totp"
+      , name_ "use_totp"
+      , type_ "checkbox"
+      ]
+    div_ [class_ "totp-zone"] $ do
+      label_ [for_ "totp"] "Two-factor code"
       input_
-        [ id_ "email"
-        , name_ "email"
-        , type_ "email"
-        , autocomplete_ "email"
-        , required_ ""
-        , placeholder_ "Email address"
-        , class_ "form-input"
-        ]
-    div_ $ do
-      label_ [for_ "password", class_ "sr-only"] "Email address"
-      input_
-        [ id_ "password"
-        , name_ "password"
-        , type_ "password"
-        , autocomplete_ "current-password"
-        , required_ ""
-        , placeholder_ "Password"
+        [ id_ "totp"
+        , name_ "totp"
+        , type_ "text"
+        , pattern_ "0-9]+"
+        , autocomplete_ "off"
         , class_ "form-input"
         ]
     div_ $
