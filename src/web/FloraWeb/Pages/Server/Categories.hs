c725ea1e392217d40ac7283abdf44b3f8b129467
Théophile Choutri <theophile@choutri.eu>
2024-02-26 10:14:28 +0100

    Convert categories handlers

--- src/web/FloraWeb/Pages/Server/Categories.hs
+++ src/web/FloraWeb/Pages/Server/Categories.hs
@@ -1,10 +1,14 @@
 module FloraWeb.Pages.Server.Categories where
 
 import Data.Text (Text)
+import Effectful (Eff, IOE, (:>))
 import Lucid (Html)
 import Network.HTTP.Types (notFound404)
-import Servant (Headers (..), ServerT)
+import Servant (Headers (..), ServerT, ServerError)
 
+import Effectful.PostgreSQL.Transact.Effect (DB)
+import Effectful.Reader.Static (Reader)
+import Flora.Environment (FeatureEnv)
 import Flora.Model.Category.Query qualified as Query
 import Flora.Model.Category.Types (Category (..))
 import Flora.Model.Package.Query qualified as Query
@@ -15,6 +19,7 @@ import FloraWeb.Pages.Templates (defaultTemplateEnv, render, templateFromSession
 import FloraWeb.Pages.Templates.Error
 import FloraWeb.Pages.Templates.Screens.Categories qualified as Template
 import FloraWeb.Types (FloraEff)
+import Effectful.Error.Static (Error)
 
 server :: SessionWithCookies (Maybe User) -> ServerT Routes FloraEff
 server sessionWithCookies =
@@ -23,13 +28,20 @@ server sessionWithCookies =
     , show = showHandler sessionWithCookies
     }
 
-indexHandler :: SessionWithCookies (Maybe User) -> FloraEff (Html ())
+indexHandler
+  :: (Reader FeatureEnv :> es, IOE :> es, DB :> es)
+  => SessionWithCookies (Maybe User)
+  -> Eff es (Html ())
 indexHandler (Headers session _) = do
   templateEnv <- templateFromSession session defaultTemplateEnv
   categories <- Query.getAllCategories
   render templateEnv $ Template.index categories
 
-showHandler :: SessionWithCookies (Maybe User) -> Text -> FloraEff (Html ())
+showHandler
+  :: (Reader FeatureEnv :> es, IOE :> es, DB :> es, Error ServerError :> es)
+  => SessionWithCookies (Maybe User)
+  -> Text
+  -> Eff es (Html ())
 showHandler (Headers session _) categorySlug = do
   templateEnv <- templateFromSession session defaultTemplateEnv
   result <- Query.getCategoryBySlug categorySlug
