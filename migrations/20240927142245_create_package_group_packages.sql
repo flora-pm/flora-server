a79aba405ed05a25837c22de8a87b71d470dfe39
Matt Roberts <mau5mat@gmail.com>
2024-09-27 17:25:54 +0100

    fix: referenced wrong property from packages

--- migrations/20240927142245_create_package_group_packages.sql
+++ migrations/20240927142245_create_package_group_packages.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS package_group_packages (
     package_group_id uuid NOT NULL REFERENCES package_groups (id)
-  , package_id uuid NOT NULL REFERENCES packages (id)
+  , package_id uuid NOT NULL REFERENCES packages (package_id)
   , PRIMARY KEY (package_group_id, package_id)
 )
