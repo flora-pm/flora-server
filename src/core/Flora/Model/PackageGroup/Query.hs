09ddc6c9e38dd81b386ada6e5f3c8ab650c2a3ef
Matt Roberts <mau5mat@gmail.com>
2024-11-10 16:39:15 +0000

    feat: added functions to `Query.hs`

--- src/core/Flora/Model/PackageGroup/Query.hs
+++ src/core/Flora/Model/PackageGroup/Query.hs
@@ -1,1 +1,20 @@
-module Flora.Model.PackageGroup.Query where
+{-# LANGUAGE QuasiQuotes #-}
+
+module Flora.Model.PackageGroup.Query
+  ( getPackagesByPackageGroupId
+  , getPackageGroupByPackageGroupName
+  ) where
+
+import Data.Text (Text)
+import Database.PostgreSQL.Entity
+import Database.PostgreSQL.Entity.Types (field)
+import Database.PostgreSQL.Simple (Only (Only))
+import Effectful
+import Effectful.PostgreSQL.Transact.Effect
+import Flora.Model.PackageGroup.Types (PackageGroup, PackageGroupId)
+
+getPackagesByPackageGroupId :: DB :> es => PackageGroupId -> Eff es (Maybe PackageGroup)
+getPackagesByPackageGroupId packageGroupId = dbtToEff $ selectById (Only packageGroupId)
+
+getPackageGroupByPackageGroupName :: DB :> es => Text -> Eff es (Maybe PackageGroup)
+getPackageGroupByPackageGroupName packageGroupName = dbtToEff $ selectOneByField [field| packageGroupName |] (Only packageGroupName)
