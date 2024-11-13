c39adc2bbb939f7de8a694e268b98b4d2fe5ccfc
Théophile Choutri <theophile@choutri.eu>
2024-11-13 19:00:44 +0100

    Display CVE summary in /security package sub-resource

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -2,7 +2,7 @@
 
 ## Contributor checklist
 
-- [ ] My PR is related to \<insert ticket number> 
+- [ ] My PR is related to \<insert ticket number>
 - [ ] I have read and understood the [CONTRIBUTING guide](https://github.com/flora-pm/flora-server/blob/development/CONTRIBUTING.md)
 - [ ] I have inserted my change and a link to this PR in the [CHANGELOG](https://github.com/flora-pm/flora-server/blob/development/CHANGELOG.md)
 - [ ] I have updated documentation in `./docs/docs` if a public feature has a behaviour change
--- test/Flora/AdvisorySpec.hs
+++ test/Flora/AdvisorySpec.hs
@@ -1,6 +1,7 @@
 module Flora.AdvisorySpec where
 
 import Advisories.Model.Advisory.Query qualified as Query
+import Advisories.Model.Affected.Query qualified as Query
 import Data.Vector qualified as Vector
 import Flora.Model.Package.Query qualified as Query
 import Flora.Model.Package.Types
@@ -10,7 +11,8 @@ spec :: TestEff TestTree
 spec =
   testThese
     "Advisory tests"
-    [ testThis "Fetching specific advisories by package id" testFetchingAllBiscuitHaskellAdvisories
+    [ testThis "Fetch advisories by package id" testFetchingAllBiscuitHaskellAdvisories
+    , testThis "Fetch advisory previews by package id" testFetchingAdvisoryPreviewByPackageId
     ]
 
 testFetchingAllBiscuitHaskellAdvisories :: TestEff ()
@@ -22,3 +24,23 @@ testFetchingAllBiscuitHaskellAdvisories = do
         (PackageName "biscuit-haskell")
   advisories <- Query.getAdvisoriesByPackageId package.packageId
   assertEqual (Vector.length advisories) 2
+
+testFetchingAllBaseAdvisories :: TestEff ()
+testFetchingAllBaseAdvisories = do
+  package <-
+    assertJust
+      =<< Query.getPackageByNamespaceAndName
+        (Namespace "haskell")
+        (PackageName "base")
+  advisories <- Query.getAdvisoriesByPackageId package.packageId
+  assertEqual (Vector.length advisories) 1
+
+testFetchingAdvisoryPreviewByPackageId :: TestEff ()
+testFetchingAdvisoryPreviewByPackageId = do
+  package <-
+    assertJust
+      =<< Query.getPackageByNamespaceAndName
+        (Namespace "haskell")
+        (PackageName "base")
+  advisories <- Query.getAdvisoryPreviewsByPackageId package.packageId
+  assertEqual (Vector.length advisories) 1
