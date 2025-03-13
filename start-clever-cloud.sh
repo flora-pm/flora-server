6129d613bfcc368a240ac0f984e1c8533edc1224
Théophile Choutri <theophile@choutri.eu>
2025-03-13 20:29:14 +0100

    Move more build in the build

--- start-clever-cloud.sh
+++ start-clever-cloud.sh
@@ -8,7 +8,6 @@ export FLORA_DB_CONNSTRING="host=${FLORA_DB_HOST} dbname=${FLORA_DB_DATABASE} po
   sslmode=${FLORA_DB_SSLMODE:-allow}"
 
 
-cabal install postgresql-migration-0.2.1.8
 make db-init
 make db-migrate
 make db-provision
