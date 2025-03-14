33fe46e3da700849db602f642c2343c7c5a5f25c
Théophile Choutri <theophile@choutri.eu>
2025-03-14 12:13:32 +0100

    Move build of postgresql-migration in build script

--- build-clever-cloud.sh
+++ build-clever-cloud.sh
@@ -7,5 +7,6 @@ export PATH="${HOME}/.ghcup/bin:${PATH}"
 mise trust
 mise install
 mise run install-haskell-toolchain
+cabal install postgresql-migration-0.2.1.8
 mise run build-js
 make build-release
--- start-clever-cloud.sh
+++ start-clever-cloud.sh
@@ -7,8 +7,6 @@ export FLORA_DB_CONNSTRING="host=${FLORA_DB_HOST} dbname=${FLORA_DB_DATABASE} po
   user=${FLORA_DB_USER} password=${FLORA_DB_PASSWORD} \
   sslmode=${FLORA_DB_SSLMODE:-allow}"
 
-
-cabal install postgresql-migration-0.2.1.8
 make db-init
 make db-migrate
 make db-provision
