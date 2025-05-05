469b729c1a7759bfb0c8c7fb0c12dd68bcfd8f5c
Théophile Choutri de Tarlé <theophile@choutri.eu>
2025-05-05 17:13:39 +0200

    Remove remnants of Souffle Datalog in CI

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -4,6 +4,3 @@ sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt -y update
 sudo apt -y install postgresql-16 g++ mcpp libffi7 llvm zlib1g-dev
-
-wget https://github.com/souffle-lang/souffle/releases/download/2.2/x86_64-ubuntu-2004-souffle-2.2-Linux.deb
-sudo dpkg -i ./x86_64-ubuntu-2004-souffle-2.2-Linux.deb
