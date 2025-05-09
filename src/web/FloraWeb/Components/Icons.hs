f988cb40625ec169be4cd301d81049df54696492
Théophile Choutri <theophile@choutri.eu>
2025-05-09 11:45:27 +0200

    Advertise package feeds in search

--- src/web/FloraWeb/Components/Icons.hs
+++ src/web/FloraWeb/Components/Icons.hs
@@ -11,6 +11,7 @@ module FloraWeb.Components.Icons
   , license
   , lightbulb
   , cross
+  , feed
   ) where
 
 import Data.Text (Text)
@@ -110,5 +111,25 @@ cross =
       <line x1="13.706511" y1="1.12859785" x2="1.1726127" y2="14.143332" id="Line-3" stroke="currentColor" stroke-width="1.2" transform="translate(7.4831, 7.7105) scale(1, -1) translate(-7.4831, -7.7105)"></line>
       <line x1="13.706511" y1="1.12859785" x2="1.1726127" y2="14.143332" id="Line-3" stroke="currentColor" stroke-width="1.2" transform="translate(7.4831, 7.7105) scale(-1, -1) translate(-7.4831, -7.7105)"></line>
   </g>
+</svg>
+    |]
+
+feed :: FloraHTML
+feed =
+  toHtmlRaw @Text
+    [str|
+<svg version="1.1" class="feed-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+	 viewBox="0 0 455.731 455.731" xml:space="preserve">
+<g>
+	<rect x="0" y="0" style="fill:#F78422;" width="455.731" height="455.731"/>
+	<g>
+		<path style="fill:#FFFFFF;" d="M296.208,159.16C234.445,97.397,152.266,63.382,64.81,63.382v64.348
+			c70.268,0,136.288,27.321,185.898,76.931c49.609,49.61,76.931,115.63,76.931,185.898h64.348
+			C391.986,303.103,357.971,220.923,296.208,159.16z"/>
+		<path style="fill:#FFFFFF;" d="M64.143,172.273v64.348c84.881,0,153.938,69.056,153.938,153.939h64.348
+			C282.429,270.196,184.507,172.273,64.143,172.273z"/>
+		<circle style="fill:#FFFFFF;" cx="109.833" cy="346.26" r="46.088"/>
+	</g>
+</g>
 </svg>
     |]
