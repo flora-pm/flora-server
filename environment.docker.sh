ef32c3dfc2db4704044b2464f5dec89142f82a40
Tixie <contact@tixie.name>
2025-09-13 14:38:08 +0200

    fix docker database host

--- environment.docker.sh
+++ environment.docker.sh
@@ -1,4 +1,4 @@
-export FLORA_DB_HOST="database"
+export FLORA_DB_HOST="flora-database"
 export FLORA_HTTP_PORT=8084
 export FLORA_DB_CONNSTRING="host=${FLORA_DB_HOST} dbname=${FLORA_DB_DATABASE}\
   user=${FLORA_DB_USER} password=${FLORA_DB_PASSWORD}"
