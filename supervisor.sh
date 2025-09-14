75b6c61178394e6e69b183ff7c5fd77b20f17c3c
Théophile Choutri <theophile@choutri.eu>
2025-09-14 10:18:08 +0200

    Revert building of assets in supervisor.sh

--- supervisor.sh
+++ supervisor.sh
@@ -4,8 +4,6 @@ set -eao pipefail
 
 [ -f ./environment.prod.sh ] && source ./environment.prod.sh || source ./environment.local.sh
 
-gmake build-assets
-
 gmake db-migrate
 
 cabal run --no-semaphore exe:flora-server
