6d92e2572fdb8c672fdd559e27662e4eb7f72640
Théophile Choutri de Tarlé <theophile+github@choutri.eu>
2025-12-27 21:10:17 +0100

    Add prometheus to docker compose environment (#968)
    * Add prometheus to docker compose environment
    
    * Include cmark-gfm in test packages
    
    * Add adequate port in the docker env file
    
    * Add changelog entry

--- environment.docker.sh
+++ environment.docker.sh
@@ -1,7 +1,10 @@
+export FLORA_DB_PORT=5432
 export FLORA_DB_HOST="flora-database"
 export FLORA_HTTP_PORT=8084
-export FLORA_DB_CONNSTRING="host=${FLORA_DB_HOST} dbname=${FLORA_DB_DATABASE}\
+export FLORA_DB_CONNSTRING="host=${FLORA_DB_HOST} port=${FLORA_DB_PORT} dbname=${FLORA_DB_DATABASE}\
   user=${FLORA_DB_USER} password=${FLORA_DB_PASSWORD}"
 export PGPASSWORD=${FLORA_DB_PASSWORD}
 
 export FLORA_PG_URI="postgresql://${FLORA_DB_USER}:${FLORA_DB_PASSWORD}@${FLORA_DB_HOST}:${FLORA_DB_PORT}/${FLORA_DB_DATABASE}"
+
+export FLORA_PROMETHEUS_ENABLED="true" # Set this variable to true or false to enable Prometheus metrics export
