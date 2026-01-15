81831a384866962e7e7b24cf02288b4a4436ccf7
Théophile Choutri de Tarlé <theophile@choutri.eu>
2026-01-15 23:02:27 +0100

    Re-generate docusaurus setup

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,98 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+const path = require("path");
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Flora Documentation',
+  tagline: '',
+  favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://flora.pm',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/documentation',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'flora-pm', // Usually your GitHub org/user name.
+  projectName: 'flora-server', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      style: 'dark',
+      title: 'Flora Documentation',
+      logo: {
+        alt: "Flora",
+        src: "img/logo-dark-background.png",
+        href: "https://flora.pm"
+      },
+      items: [
+        {
+          type: "doc",
+          docId: "intro",
+          label: "Guides",
+          position: "left",
+        },
+        {
+          to: "/api-reference",
+          label: "API Reference",
+          position: "left",
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} Flora.pm. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  plugins: [path.resolve(__dirname, "redoc-plugin")],
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
