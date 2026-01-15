570007018aa7b9aa30f8d69a6e8568e2456ef699
Théophile Choutri de Tarlé <theophile@choutri.eu>
2026-01-15 23:26:29 +0100

    docs-only mode

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -41,6 +41,7 @@ const config: Config = {
       'classic',
       {
         docs: {
+          routeBasePath: "/",
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
