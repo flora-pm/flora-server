3f7778c952504401e6ea557254df47c25705acd8
Théophile Choutri de Tarlé <theophile@choutri.eu>
2026-04-29 22:02:25 +0200

    Fix typo in migration

--- migrations/20260429194233_delete_odd_jobs.sql
+++ migrations/20260429194233_delete_odd_jobs.sql
@@ -1,7 +1,1 @@
 DROP TABLE IF EXISTS oddjobs;
-
-DROP FUNCTION IF EXISTS notify_job_monitor_for_oddjobs;
-
-DROP SEQUENCE IF EXISTS oddjobs_id_seq;
-
-DROP TRIGGER IF EXISTS trg_notify_job_monitor_for_oddjobsb;
