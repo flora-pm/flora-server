a0f68e5041269e448042dc17cff83d96f1c39c72
Tixie <contact@tixie.name>
2026-05-07 16:52:42 +0200

    Set up standardized text size and radius values

--- assets/style-tokens/tokens.js
+++ assets/style-tokens/tokens.js
@@ -1,9 +1,13 @@
 const colors = require("./colors.json");
 const fonts = require("./fonts.json");
 const spacing = require("./spacing.json");
+const radius = require("./radius.json");
+const texts = require("./texts.json");
 
 module.exports = {
 	"color": colors,
 	"font": fonts,
 	"space": spacing,
+	"radius": radius,
+	"text": texts
 }
