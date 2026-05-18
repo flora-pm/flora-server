044448e247f1aa9c5fea9d48f7c3ef67ac19dd80
Théophile Choutri <theophile@choutri.eu>
2026-05-18 15:13:26 +0200

    style

--- src/core/Flora/Model/PackageUploader/Guard.hs
+++ src/core/Flora/Model/PackageUploader/Guard.hs
@@ -1,14 +1,14 @@
 module Flora.Model.PackageUploader.Guard where
 
+import Data.Text
 import Effectful
 import Effectful.PostgreSQL.Transact.Effect
 import Effectful.Trace
 import Monitor.Tracing qualified as Tracing
 
-import Flora.Model.PackageUploader.Types
-import Flora.Model.PackageUploader.Query qualified as Query
-import Data.Text
 import Flora.Model.PackageIndex.Types
+import Flora.Model.PackageUploader.Query qualified as Query
+import Flora.Model.PackageUploader.Types
 
 guardThatPackageUploaderExists
   :: (DB :> es, Trace :> es)
