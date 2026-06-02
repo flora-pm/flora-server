2f86af118b51c0c9678484ba6e9e47d7d355bf9c
Tixie <contact@tixie.name>
2026-06-02 19:26:51 +0200

    WIP: Moar todo in package header
    # Conflicts:
    #	src/web/FloraWeb/Pages/Templates/Screens/Packages.hs

--- src/web/FloraWeb/Pages/Templates/Screens/Packages.hs
+++ src/web/FloraWeb/Pages/Templates/Screens/Packages.hs
@@ -1,5 +1,6 @@
 module FloraWeb.Pages.Templates.Screens.Packages where
 
+import Control.Monad
 import Data.Function ((&))
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
@@ -49,9 +50,9 @@ showPackage
   categories
   groups
   activeMaintainers
-  mUploader =
-    div_ [class_ "container"] $ do
-      presentationHeader latestRelease namespace name latestRelease.synopsis groups
+  mUploader = do
+    presentationHeader latestRelease namespace name latestRelease.synopsis groups
+    div_ [class_ "wrapper inset-large"] $ do
       packageBody
         package
         packageIndexURL
@@ -76,7 +77,7 @@ presentationHeader release namespace name synopsis groups =
             span_ [class_ "prefix"] $ do
               -- TODO: Fix link
               a_ [href_ (display namespace)] (toHtml $ display namespace)
-              toHtmlRaw ("&ThinSpace;/&ThinSpace;" :: Text)
+              (toHtmlRaw ("&ThinSpace;/&ThinSpace;" :: Text))
             toHtml name
           p_ [class_ "pageHead-subtitle text-break"] (toHtml synopsis)
         div_ [class_ "flow flow--small self-center"] $ do
@@ -91,28 +92,31 @@ presentationHeader release namespace name synopsis groups =
             --   "Deprecated"
             span_ [class_ "title-2 text-right leading-thin"] $ toHtml release.version
       div_ [class_ "pageHead-tip"] $ do
+        -- TODO: Split tabs in a separate function
+        -- TODO: Display actual link, labels, and current attribute/class
         nav_ [class_ "tabs", id_ "subsections", ariaLabel_ "Package sections"] $ do
           a_ [class_ "tab", href_ "/", ariaCurrent_ "page"] $ do
             Icons.bookOpenText
             "About"
           a_ [class_ "tab", href_ "/"] $ do
             Icons.history
-            "49 Versions"
+            "49 Versions" -- TODO: Display real number
           a_ [class_ "tab", href_ "/"] $ do
             Icons.logs
             "Changelog"
           a_ [class_ "tab", href_ "/"] $ do
             Icons.folderTree
-            "1 Dependency"
+            "1 Dependency" -- TODO: Display real number + dependency/dependencies
           a_ [class_ "tab", href_ "/"] $ do
             Icons.packageSearch
-            "5 Dependents"
+            "5 Dependents" -- TODO: Display real number + dependent/dependent
         div_ [class_ "tabs-mobile", id_ "subsectionsMobile"] $ do
+          -- TODO: Display current section in aria-label attribute
           button_ [class_ "tabs-mobileBtn btn btn--secondary", ariaLabel_ ("Switch section (Current: " <> "About" <> ")"), popovertarget_ "subsectionsMobile-menu"] $ do
             Icons.bookOpenText
             div_ [class_ "flex-grow"] $ do
-              div_ [class_ "prefix"] "Current section"
-              div_ "About"
+              div_ [class_ "prefix"] $ "Current section"
+              div_ $ "About" -- TODO: Display current section label
             Icons.chevronUpDown
           nav_ [class_ "dropdown dropdown--full", id_ "subsectionsMobile-menu", ariaLabel_ "Package sections", popover_ ""] $ do
             a_ [class_ "dropdown-item dropdown-item--current", href_ "/", ariaCurrent_ "page"] $ do
@@ -120,16 +124,16 @@ presentationHeader release namespace name synopsis groups =
               "About"
             a_ [class_ "dropdown-item", href_ "/"] $ do
               Icons.history
-              "49 Versions"
+              "49 Versions" -- TODO: Display real number
             a_ [class_ "dropdown-item", href_ "/"] $ do
               Icons.logs
               "Changelog"
             a_ [class_ "dropdown-item", href_ "/"] $ do
               Icons.folderTree
-              "1 Dependency"
+              "1 Dependency" -- TODO: Display real number + dependency/dependencies
             a_ [class_ "dropdown-item", href_ "/"] $ do
               Icons.packageSearch
-              "5 Dependents"
+              "5 Dependents" -- TODO: Display real number + dependent/dependent
 
 packageBody
   :: Package
@@ -157,15 +161,15 @@ packageBody
   numberOfDependents
   categories
   mLotteryFactor
-  mUploader =
-    div_ [class_ "package-body"] $ do
-      div_ [class_ "package-left-column"] $ ul_ [class_ "package-left-rows"] $ do
+  mUploader = do
+    h2_ [class_ "sr-only"] "About"
+    div_ [class_ "package-about aside aside--reverse aside--start"] $ do
+      div_ [class_ "package-details"] $ do
         displayCategories categories
         displayLicense license
         displayMaintainer namespace packageName mLotteryFactor mUploader maintainer
         displayLinks namespace packageName packageIndexURL latestRelease
         displayVersions namespace packageName packageReleases numberOfReleases
-      div_ [class_ "package-right-column"] $ ul_ [class_ "package-right-rows"] $ do
         case deprecationInfo of
           Just inFavourOf -> displayPackageDeprecation inFavourOf
           Nothing ->
@@ -176,7 +180,9 @@ packageBody
         displayDependencies (namespace, packageName, version) numberOfDependencies dependencies
         displayDependents (namespace, packageName) numberOfDependents dependents
         displayPackageFlags flags
-      div_ [class_ "release-readme-column"] $ div_ [class_ "release-readme"] $ displayReadme latestRelease
+      section_ [class_ "flow"] $ do
+        h3_ [class_ "title-section"] "Readme"
+        div_ [class_ "prose"] $ displayReadme latestRelease
 
 getLatestViableRelease
   :: Namespace
