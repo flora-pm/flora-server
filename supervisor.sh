e6e80dba4b54398fb127d7fa7aadec87ffd6d1f5
Théophile Choutri <theophile@choutri.eu>
2026-07-15 12:11:40 +0200

    Remove stale files

--- supervisor.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eao pipefail
-
-[ -f ./environment.prod.sh ] && source ./environment.prod.sh || source ./environment.local.sh
-
-gmake db-migrate
-
-cabal run --no-semaphore exe:flora-server
